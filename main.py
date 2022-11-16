8340e5131cf6a93e0618f5a04a1bbb77250ce619
Philipp Peron <philipp.peron@gmail.com>
2022-11-16 11:19:41 -0500

    Add create_ec2 function (From previous assignment)

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
 """Script to create and benchmark a standalone MySQL server against a MySQL server cluster."""
+
+import boto3
+
+
+def create_ec2(instance_type, sg_id, key_name):
+    """Creates an EC2 instance
+
+    Args:
+        instance_type (str): Instance type (m4.large, ...)
+        sg_id (str): Security group ID
+        key_name (str): SSH key name
+
+    Returns:
+        instance: The created instance object
+    """
+    instance = EC2_RESOURCE.create_instances(
+        ImageId='ami-0149b2da6ceec4bb0',
+        MinCount=1,
+        MaxCount=1,
+        InstanceType=instance_type,
+        Monitoring={'Enabled': True},
+        SecurityGroupIds=[sg_id],
+        KeyName=key_name,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': 'lab2-hadoop-instance'
+                    },
+                ]
+            },
+        ]
+    )[0]
+    print(f'{instance} is starting')
+    return instance
